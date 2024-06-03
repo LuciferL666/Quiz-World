3ce8ec43129e9ed1cfd0db5ed8ea95fa9883bb3a
PeykoGeorgiev <peyko92gm@gmail.com>
2024-06-03 16:43:59 +0200

    put real anwers in li

--- Quiz-World/src/Components/basicQuizQuestions/BasicQuiz.jsx
+++ Quiz-World/src/Components/basicQuizQuestions/BasicQuiz.jsx
@@ -10,6 +10,11 @@ const BasicQuiz = () => {
     const checkAns = (e, ans) => {
         if(question.ans === ans) {
             e.target.classList.add('correct')
+    const checkAns = (e,ans) => {
+            if (question.ans===ans) {
+                e.target.classList.add('correct');
+            } else {
+                e.target.classList.add('wrong');
         }
     }
     return (
@@ -18,10 +23,10 @@ const BasicQuiz = () => {
             <hr />
             <h2>{index + 1}. {question.question}</h2>
             <ul>
-                <li>{question.option1}</li>
-                <li>{question.option2}</li>
-                <li>{question.option3}</li>
-                <li>{question.option4}</li>
+                <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+                <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li> 
+                <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+                <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
             </ul>
             <button>Next</button>
             <div className="index">1 of 10 questions</div>
