d9d530dd1f986918a06d1d4e44920a28be32f81e
PeykoGeorgiev <peyko92gm@gmail.com>
2024-06-03 17:00:50 +0200

    show correct when click wrong answer

--- Quiz-World/src/Components/basicQuizQuestions/BasicQuiz.jsx
+++ Quiz-World/src/Components/basicQuizQuestions/BasicQuiz.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './BasicQuiz.css';
 import { basic } from "../../../public/Questions/basicQ";
+import { useRef } from "react";
 
 const BasicQuiz = () => {
 
@@ -8,7 +9,12 @@ const BasicQuiz = () => {
     let [question, setQuestion] = useState(basic[index]);
     let [lock, setLock] = useState(false);
 
-    let
+    let Option1 = useRef(null);
+    let Option2 = useRef(null);
+    let Option3 = useRef(null);
+    let Option4 = useRef(null);
+
+    let option_array = [Option1, Option2, Option3, Option4];
 
     const checkAns = (e,ans) => {
         if (lock === false){
@@ -18,6 +24,7 @@ const BasicQuiz = () => {
             } else {
                 e.target.classList.add('wrong');
                 setLock(true)
+                option_array[question.ans - 1].current.classList.add('correct')
             }
         }
     }
@@ -27,10 +34,10 @@ const BasicQuiz = () => {
             <hr />
             <h2>{index + 1}. {question.question}</h2>
             <ul>
-                <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
-                <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li> 
-                <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
-                <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+                <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+                <li ref={Option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li> 
+                <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+                <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
             </ul>
             <button>Next</button>
             <div className="index">1 of 10 questions</div>
